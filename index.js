08552cb2794d88ede7d2ec00893df0b3e9829894
Novaenn <samuele.bella@hotmail.it>
2023-09-13 17:33:06 +0200

    Initial submit commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const fs = require('fs')
 
 const PORT = 3000;
 
@@ -10,22 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  var data = "";
-
   if (!req.body.password) {
     console.log('Received username:', username);
-    data = !"Username: " + username;
   };
   if (!req.body.username) {
     console.log('Received password:', password);
-    data = "Password: " + password + "\n";
   };
-
-  // Write data in 'Credenziali.txt' .
-  fs.appendFile('Credenziali.txt', data, (err) => {
-      // In case of a error throw err.
-      if (err) throw err;
-  })
 });
 
 app.listen(PORT, () => {
